28cee09c12f92beccb1d97768c98716a9c86e186
HHannah <58073774+neosynthetica@users.noreply.github.com>
2022-06-27 15:49:56 +1200

    fed up

--- style.js
+++ style.js
@@ -1,8 +1,11 @@
-function makeStyle(style){
-    
-}
 // check to see what the current stylesheet is
+var styName = document.getElementsByClassName('style').href.substr(6,4)
 
+function makeStyle(style){
+    document.getElementsByClassName('style').href="style_"+style+".css";
+}
+
+makeStyle();
 // check to see if the button has been pressed
 
 // function to change the stylesheet
